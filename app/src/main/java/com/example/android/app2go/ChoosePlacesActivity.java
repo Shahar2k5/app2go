267ed6eb258f4c5a623a5f975850fe4d573ade2b
bar <baryariv@gmail.com>
2016-03-03 15:58:02 +0200

    Start LocationActivity

--- app/src/main/java/com/example/android/app2go/ChoosePlacesActivity.java
+++ app/src/main/java/com/example/android/app2go/ChoosePlacesActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.app2go;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
@@ -7,6 +8,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
@@ -14,17 +23,25 @@ import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.gson.Gson;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class ChoosePlacesActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener {
     private static final String TAG = ChoosePlacesActivity.class.getSimpleName();
+    private static final float DEFAULT_BACKOFF_MULT = 1.0f;
+    private static final String URL = "http://195.28.181.78:83/api/Navigation/Complex";
 
     private Navigation navigation;
-    private List<String> addresses;
     private GoogleApiClient mGoogleApiClient;
     private boolean isFirstAddress;
+    private JSONObject serverResponse;
 
     private PlaceAutocompleteFragment autocompleteFragment;
     private TextView startNavigationBtn;
@@ -36,8 +53,8 @@ public class ChoosePlacesActivity extends FragmentActivity implements GoogleApiC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_places);
         navigation = new Navigation();
-        addresses = new ArrayList<>();
         isFirstAddress = true;
+        serverResponse = new JSONObject();
 
         mGoogleApiClient = new GoogleApiClient
                 .Builder(this)
@@ -56,17 +73,14 @@ public class ChoosePlacesActivity extends FragmentActivity implements GoogleApiC
             @Override
             public void onPlaceSelected(Place place) {
                 Log.i(TAG, "Place: " + place.getName());
-                if(isFirstAddress) {
+                if (isFirstAddress) {
                     navigation.addStartAddress(place.getName().toString());
                     isFirstAddress = false;
-                }
-                else {
-                    if(navigation.getEndAdd() != null && !navigation.getEndAdd().isEmpty()) {
-//                        addresses.add(navigation.getEndAdd());
+                } else {
+                    if (navigation.getEndAdd() != null && !navigation.getEndAdd().isEmpty()) {
                         navigation.addAddress(navigation.getEndAdd());
                         navigation.addEndAdd(place.getName().toString());
-                    }
-                    else{
+                    } else {
                         navigation.addEndAdd(place.getName().toString());
                     }
                 }
@@ -85,15 +99,16 @@ public class ChoosePlacesActivity extends FragmentActivity implements GoogleApiC
         startNavigationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String add = "";
-                for(int i = 0; i < navigation.getAddresses().size(); i++){
-                    add += navigation.getAddresses().get(i) + ", ";
+                try {
+                    getRoute(navigation, URL);
+                    Log.i(TAG, serverResponse.toString());
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-                String s = "Start address: " + navigation.getStartAdd() + "\n"
-                        + add
-                        + " End address: " + navigation.getEndAdd();
-
-                addressesTxt.setText(s);
             }
         });
     }
@@ -101,5 +116,54 @@ public class ChoosePlacesActivity extends FragmentActivity implements GoogleApiC
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Log.e("ChhsePlaceesActivity", "Connection failed");
+    }
+
+    public void getRoute(Navigation nav, String url) throws ExecutionException, InterruptedException, JSONException {
+        final long start = System.nanoTime();
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        Map<String, String> params = new HashMap<String, String>();
+        Gson gson = new Gson();
+        String jsonReq = gson.toJson(nav);
+        params.put("navigation", jsonReq);
+
+        JSONObject json = new JSONObject(jsonReq);
+
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url, json, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                long elapsedTime = System.nanoTime() - start;
+                Log.i("Time", String.valueOf(elapsedTime / 1000000000) + " Sec");
+                serverResponse = response;
+                Intent intent = new Intent(ChoosePlacesActivity.this, LocationsActivity.class);
+                intent.putExtra("json", (Serializable) serverResponse);
+                startActivity(intent);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error", error.toString());
+                try {
+                    serverResponse = new JSONObject("{\"optimizedRoute\":[{\"source\":\"כרם התימנים 12, תל אביב יפו\",\"destination\":\"גלבוע 11, כוכב יאיר צור יגאל\",\"endLatitude\":0,\"endLongitude\":0,\"duration\":2570,\"durationText\":\"43 mins\"},{\"source\":\"גלבוע 11, כוכב יאיר צור יגאל\",\"destination\":\"רוטשילד 20, כפר סבא\",\"endLatitude\":0,\"endLongitude\":0,\"duration\":1186,\"durationText\":\"20 mins\"},{\"source\":\"רוטשילד 20, כפר סבא\",\"destination\":\"כרם התימנים 12, תל אביב יפו\",\"endLatitude\":0,\"endLongitude\":0,\"duration\":2152,\"durationText\":\"36 mins\"}],\"totalTime\":5908}");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(ChoosePlacesActivity.this, LocationsActivity.class);
+                intent.putExtra("json", (Serializable) serverResponse);
+                startActivity(intent);
+            }
+        }) {
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<>();
+
+                return headers;
+            }
+        };
+        req.setTag("");
+        req.setRetryPolicy(new DefaultRetryPolicy(5000, 3, DEFAULT_BACKOFF_MULT));
+        queue.add(req);
     }
 }
